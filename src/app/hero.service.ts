a4dfb0f44d636ee887254e306538de0567339121
Marlon Gonzalez Gongora <nickoo1987@gmail.com>
2018-02-20 09:27:48 -0300

    showing messages using services

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,15 +3,20 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
+import { MessageService } from './message.service';
 
 
 @Injectable()
 export class HeroService {
 
   getHeroes(): Observable<Hero[]> {
+    // Todo: send the message _after_ fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
 
-  constructor() { }
+  //This is a typical "service-in-service" scenario: you inject the MessageService into the HeroService which is
+  // injected into the HeroesComponent.
+  constructor(private messageService: MessageService) { }
 
 }
